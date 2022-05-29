0d6be045492078ac7c73645398e257884293f28c
idanb-giniapps <idanb@gini-apps.com>
2022-05-29 20:07:47 +0700

    Initial Commit

---
+++ SuperSecureCloud_Start/SuperSecureCloud_StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSecureCloud_StartApp.swift
+//  SuperSecureCloud_Start
+//
+//  Created by Idan Birman on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperSecureCloud_StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
